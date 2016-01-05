806a8be06f973e29eda261c40f54711c02b57d4b
Nicholas Neumann-Chun <nicholas.babelthuap (at) gmail.com>
2016-01-05 11:16:33 -0800

    Improve code formatting

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-$(document).ready(function(){
-    // set last modified date
-    var modified = document.lastModified; // -> "MM/DD/YYYY hr:min:sec"
-    modified = modified.slice(0, modified.indexOf(' '));
-    $('#last-modified').text(modified);
+'use strict';
 
-    // set copyright date
-    var today = new Date();
-    var year = today.getFullYear();
-    $('#year').text(year == 2015 ? year : "2015-" + year);
+$(document).ready(function() {
+  // set last modified date
+  var modified = new Date(document.lastModified);
+  $('#last-modified').text(modified.toLocaleDateString());
+
+  // set copyright date
+  var today = new Date();
+  var year = today.getFullYear();
+  $('#year').text(year == 2015 ? year : '2015-' + year);
 });
