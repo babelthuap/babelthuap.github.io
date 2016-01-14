6d8e0aacf32b63c7d0ee96fb521f1f2257c63c59
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-01-13 16:44:29 -0800

    Add navbar

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 'use strict';
 
-// use this function in place of jQuery(document).ready(...)
-!function(fn) {
-  if (document.readyState != 'loading') {
-    fn();
-  } else {
-    document.addEventListener('DOMContentLoaded', fn);
-  }
-}(init);
-
-function init() {
+$(document).ready(function() {
   // set last modified date
   var modified = new Date(document.lastModified);
-  document.getElementById('last-modified').innerHTML = modified.toLocaleDateString();
+  $('#last-modified').text(modified.toLocaleDateString());
 
   // set copyright date
   var today = new Date();
   var year = today.getFullYear();
-  document.getElementById('year').innerHTML = (year == 2015 ? year : '2015-' + year);
-}
+  $('#year').text(year == 2015 ? year : '2015-' + year);
+});
