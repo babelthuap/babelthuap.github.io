280dfda6ddbe431231c6eee378eb9bfab4dcc90e
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2016-04-19 14:16:19 -0500

    Remove contact info

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// ping Friend Finder
-$.get('http://young-favorite-users.herokuapp.com');
-// ping Green it!
-$.get('https://vast-sierra-7757.herokuapp.com/');
-
 $(document).ready(function() {
   // keep thumbnails square
   var $thumbnails = $('.thumbnail');
@@ -22,12 +17,4 @@ $(document).ready(function() {
   var today = new Date();
   var year = today.getFullYear();
   $('#year').text(year == 2015 ? year : '2015-' + year);
-
-  // set email
-  var $email = $('<a>').attr('href', 'mailto:nicholas.babelthuap@gmail.com')
-                   .text('nicholas.babelthuap (at) gmail.com');
-  $('#mah-email').append($email);
-
-  // set phone number
-  $('#mah-numba').text('651-491-4928');
 });
